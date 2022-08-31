1114390f7c79f21e6a1bccd5fb1056fd57ebe62b
LeoMouyna <leo.mouyna@contentsquare.com>
2022-08-31 16:58:46 +0200

    feat: add dom manipulation example

--- src/background.ts
+++ src/background.ts
@@ -7,6 +7,37 @@ function updateBackgroundColor(color: string): void {
   document.body.style.backgroundColor = color;
 }
 
+function getAllTextElement(): HTMLElement[] {
+  const relevantTags: (keyof HTMLElementTagNameMap)[] = [
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "p",
+  ];
+  const collections = relevantTags.map((tag) =>
+    document.getElementsByTagName(tag)
+  );
+  const list = collections.flatMap((collection) =>
+    new Array<HTMLCollectionOf<HTMLElement>>(collection.length)
+      .fill(collection)
+      .map((collection, index) => collection.item(index))
+      .filter((element): element is HTMLElement => element !== null)
+  );
+  return list;
+}
+
+function changeLetterToAnOther(source: string, target: string) {
+  const elements = getAllTextElement();
+  elements.map(
+    (element) =>
+      (element.textContent =
+        element.textContent?.replace(source, target) ?? null)
+  );
+}
+
 chrome.runtime.onInstalled.addListener(async () => {
   await chrome.storage.local.set({ color: green, active });
   console.log("Default background color set to %cgreen", `color: ${green}`);
@@ -23,6 +54,11 @@ chrome.action.onClicked.addListener(async (tab) => {
         target: { tabId: tab.id ? tab.id : -1 },
         func: updateBackgroundColor,
         args: [color],
+      }),
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id ? tab.id : -1 },
+        func: changeLetterToAnOther,
+        args: ["a", "A"],
       }),
     ]);
   } catch (e) {
